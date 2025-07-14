8f19132280eec44af8fe559125f5bcf0e3a876a7
AkumuOmolo <akumu.omoloh@gmail.com>
2025-07-14 18:50:21 +0300

    Adds src/components/Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
@@ -11,6 +12,20 @@ function Question({ question, onAnswered }) {
   }
 
   const { id, prompt, answers, correctIndex } = question;
+
+  useEffect (() => {
+    if (timeRemaining > 0) {
+      const timerId = setTimeout (() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+
+    } else {
+      setTimeRemaining(10);
+      onAnswered(false)
+    }
+  }, [timeRemaining]);
+
 
   return (
     <>
